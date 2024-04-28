aea681a38c5079a186df20c1ad57a6a8f7f84323
Lucas <Lucas@DESKTOP-AIA1REJ>
2024-04-28 16:52:13 -0300

    TPN2EJERCICIO2

---
+++ src/TPN2EJERCICIO2/ENTEROSYDECIMALES.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TPN2EJERCICIO2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Lucas
+ */
+public class ENTEROSYDECIMALES {
+    public static void main(String[] args) {
+                Scanner sc =new Scanner (System.in);
+                OPERACIONES obj = new OPERACIONES();
+                
+                int n1entero, n2entero;
+                double n1decimal, n2decimal;
+                
+                
+                //NUMEROS ENTEROS
+                System.out.println("Ingrese el Primer numero entero");
+                n1entero = sc.nextInt();
+                System.out.println("Ingrese el Segundo numero entero");
+                n2entero = sc.nextInt(); 
+                //SUMA DE NUMEROS ENTEROS
+                obj.setDatosnumerosenteros(n1entero, n2entero);
+                System.out.println("La Suma de los dos numeros enteros son: " +obj.sumaenteros());
+                //RESTA DE NUMEROS ENTEROS 
+                obj.setDatosnumerosenteros(n1entero, n2entero);
+                System.out.println("La Resta de los dos numeros enteros son: " +obj.restaenteros());
+                //MULTIPLICACION DE NUMEROS ENTEROS
+                obj.setDatosnumerosenteros(n1entero, n2entero);
+                System.out.println("La Multiplicacion de los dos numeros enteros son: " +obj.multiplicacionenteros());
+                //DIVISION DE NUMEROS ENTEROS
+                obj.setDatosnumerosenteros(n1entero, n2entero);
+                System.out.println("La Division de los dos numeros enteros son: " +obj.divisionenteros());
+                
+                
+                //NUMEROS DECIMALES
+                System.out.println("Ingrese el Primer numero decimal");
+                n1decimal = sc.nextDouble();
+                System.out.println("Ingrese el Segundo numero decimal");
+                n2decimal = sc.nextDouble(); 
+                //SUMA DE NUMEROS DECIMALES
+                obj.setDatosnumerosDecimales(n1decimal, n2decimal); 
+                System.out.println("La Suma de los dos numeros decimales son: " +obj.sumadecimales());
+                //RESTA DE NUMEROS DECIMALES 
+                obj.setDatosnumerosDecimales(n1decimal, n2decimal); 
+                System.out.println("La Resta de los dos numeros decimales son: " +obj.restadecimales());
+                //MULTIPLICACION DE NUMEROS DECIMALES 
+                obj.setDatosnumerosDecimales(n1decimal, n2decimal); 
+                System.out.println("La Multiplicacion de los dos numeros decimales son: " +obj.multiplicaciondecimales());
+                //DIVISION DE NUMEROS DECIMALES 
+                obj.setDatosnumerosDecimales(n1decimal, n2decimal); 
+                      System.out.println("La Division de los dos numeros decimales son: " +obj.divisiondecimales());
+    }
+}    
+
