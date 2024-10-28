f180a1dc26270cdc730d5303f793032ec03ad51e
Lucas <Lucas@DESKTOP-AIA1REJ>
2024-10-27 22:48:18 -0300

    lol

---
+++ src/ARCHIVOSINDEXADOS/ARCHIVOSDEACCESOALEATORIO/ARCHIVOSDETEXTO/ARCHIVOSBINARIOS/ARCHIVOSECUENCIALES/ESCRIBIR.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ARCHIVOSINDEXADOS.ARCHIVOSDEACCESOALEATORIO.ARCHIVOSDETEXTO.ARCHIVOSBINARIOS.ARCHIVOSECUENCIALES;
+import java.io.*;
+
+/**
+ *
+ * @author Lucas
+ */
+public class ESCRIBIR {
+    public static void main(String[] args) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("archivo.txt"))) {
+            bw.write("Línea 1");
+            bw.newLine();  // Añadir un salto de línea
+            bw.write("Línea 2");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
---
+++ src/ARCHIVOSINDEXADOS/ARCHIVOSDEACCESOALEATORIO/ARCHIVOSDETEXTO/ARCHIVOSBINARIOS/ARCHIVOSECUENCIALES/LEER.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ARCHIVOSINDEXADOS.ARCHIVOSDEACCESOALEATORIO.ARCHIVOSDETEXTO.ARCHIVOSBINARIOS.ARCHIVOSECUENCIALES;
+ import java.io.*;
+
+/**
+ *
+ * @author Lucas
+ */
+public class LEER {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new FileReader("archivo.txt"))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
