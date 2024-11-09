ccd12ef4303b9e275db21f261c8c200edc729308
Lucas <Lucas@DESKTOP-AIA1REJ>
2024-11-09 16:12:09 -0300

    ERERERER

---
+++ src/EXCEPCIONES/MAIN.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EXCEPCIONES;
+
+/**
+ *
+ * @author Lucas
+ */
+public class MAIN {
+    public static void main (String [] args){
+        
+    try{
+        int num1=5,num2=0;
+        
+        int resultado = num1/num2;
+          
+        
+    }    
+    catch(Exception g){
+         System.out.println("no se puede dividr en cero");
+    }
+        
+        
+        
+        
+    }
+}
